f85f0b3b5f2e610760fe95d75353c87e802ea2c2
Michal Charemza <michal@charemza.name>
2019-05-20 07:10:10 +0100

    (tests) Latest lowhaio

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ class TestIntegration(unittest.TestCase):
         await site.start()
 
         request, close = Pool()
-        self.add_async_cleanup(close)
+        self.addCleanup(close)
 
         async def chunked_data(chunk_size):
             chunks = [data[i:i+chunk_size] for i in range(0, len(data), chunk_size)]
